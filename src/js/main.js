146765ae078ffe7ba778454d137e56fe63a4c605
ShirleyRM <srm.shirley@gmail.com>
2025-09-12 22:55:39 -0500

    added drag and drop function to the avatar box

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+const uploadBox = document.getElementById("upload-box");
+const fileInput = document.getElementById("avatar");
+
+function handleFile(file) {
+  if (file) {
+    console.log("file uploaded:", file.name);
+  }
+}
+
+// click the uploadBox - open input file
+uploadBox.addEventListener("click", () =>{
+  fileInput.click();
+});
+
+
+// select the file from input
+fileInput.addEventListener("change", () => {
+  handleFile(fileInput.files[0]);
+});
+
+//when drag the file above the box
+uploadBox.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  uploadBox.classList.add("dragover");
+});
+
+//when you close the box
+uploadBox.addEventListener("dragleave", () => {
+  uploadBox.classList.remove("dragover");
+});
+
+//when you drop the file
+uploadBox.addEventListener("drop", (e) => {
+  e.preventDefault();
+  uploadBox.classList.remove("dragover");
+
+  const file = e.dataTransfer.files[0];
+  handleFile(file);
+});
